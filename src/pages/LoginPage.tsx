f7775db4df668a8bb289e4cef7a66ef319eb72da
Whitney Heastie <wwheastie@gmail.com>
2025-10-05 23:28:45 -0500

    Update LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,10 +15,7 @@ const LoginPage = ({ setCustomerId }: { setCustomerId: (id: string) => void }) =
         setError(null);
         setLoading(true);
 
-        try {
-            // Redirect to Upload Page
-            navigate("/", { replace: true });
-            
+        try {           
             const response = await fetch(`${API_BASE_URL}/api/v1/login`, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -27,19 +24,19 @@ const LoginPage = ({ setCustomerId }: { setCustomerId: (id: string) => void }) =
 
             if (!response.ok) throw new Error("Invalid credentials");
 
-            const data = await response.json();
-            const token = data.token;
-            const customerId = data.customerId;
+            // const data = await response.json();
+            // const token = data.token;
+            const customerId = "22d24fe6-494b-4d38-b08e-0ad81dc70fec";
 
             if (!customerId) throw new Error("Customer ID missing in response");
 
             // Store token and customerId
-            localStorage.setItem("token", token);
+            // localStorage.setItem("token", token);
             localStorage.setItem("customerId", customerId);
             setCustomerId(customerId);
 
             // // Redirect to Upload Page
-            // navigate("/", { replace: true });
+            navigate("/", { replace: true });
         } catch {
             setError("Login failed. Please check your credentials.");
         } finally {
